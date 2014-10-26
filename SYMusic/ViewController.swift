e3450571163add6dafb66d6da42efe5241dea021
vale <changweitu@gmail.com>
2014-10-26 08:53:18 +0800

    Initial Commit

---
+++ SYMusic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SYMusic
+//
+//  Created by vale on 10/26/14.
+//  Copyright (c) 2014 changweitu@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
