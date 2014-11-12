9e3653a9381f9222598357cce380a9038aaa81a8
vale <changweitu@gmail.com>
2014-11-12 21:46:34 +0800

    setup views using size classes and auto layout.

--- SYMusic/ViewController.swift
+++ SYMusic/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        blurImgView .setImageToBlur(UIImage(named: "firstLoadedImg.jpg"), blurRadius: kLBBlurredImageDefaultBlurRadius) { () -> Void in
+//            
+//            print("finish");
+//        }
         
     }
 
